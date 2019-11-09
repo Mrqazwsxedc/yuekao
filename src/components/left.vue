43bfd32ba807c496b080564718fe7d0656597686
yang <470218923@qq.com>
2019-11-09 08:55:57 +0800

    '1'

---
+++ src/components/left.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="left">
+        <ul>
+            <li v-for="(item,index) in list" :key="index"
+            @click="changeInd(index)">{{item.name}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        list:{
+            type:Array
+        }
+    },
+    created(){
+         console.log(this.list)
+    },
+    methods:{
+        changeInd(ind){
+            this.$emit('changeind',ind);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Left :list="list" @changeind="change"/>
+    <Right :list="list[ind].arr"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Left from '@/components/left.vue';
+import Right from '@/components/right.vue'
 export default {
   name: 'home',
+  data(){
+    return {
+      list:[
+        {
+          name:'哈哈哈',
+          arr:[
+            {
+              id:'1',
+              text:'大飞',
+              num:1
+            },
+            {
+              id:'2',
+              text:'大红',
+              num:2
+            }
+          ]
+        },
+         {
+          name:'代发价',
+          arr:[
+            {
+              id:'1',
+              text:'未多少',
+              num:1
+            },
+            {
+              id:'2',
+              text:'总共的',
+              num:2
+            }
+          ]
+        }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    // HelloWorld
+    Left,
+    Right
   }
 }
 </script>
